28c40f01c61aa96276f76d9506b179608a6fbd58
triggleman <toddriggleman@gmail.com>
2019-09-25 22:59:09 -0400

    trying to get api to wrk

--- main.js
+++ main.js
@@ -1,14 +1,27 @@
 $(document).ready(function() {
     console.log('this is running');
 
+
     $('#search').on('click', (e) => {
         e.preventDefault();
-        const location = $('#location').value();
-        console.log(location);
+        const loc = $('#location').val();
+        console.log(loc);
+        apiCall(loc);
+        console.log('api call');
         
     });
     
-
+    function apiCall (location){
+        //const URL = 'api.openweathermap.org/data/2.5/weather?zip=' + location + ',us&appid=bc2980500f68c229827d6af054131740';
+        $.ajax({
+            url: 'api.openweathermap.org/data/2.5/weather?zip=' + location + ',us&appid=bc2980500f68c229827d6af054131740',
+            type: 'GET'
+            }.done(data => {
+                console.log(data);
+                
+            })
+        })
+    };
 
 
 
